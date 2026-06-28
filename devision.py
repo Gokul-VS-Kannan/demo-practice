447326c2553d754e89e966a22a473205f97dfdb2
Gokul-VS-Kannan <gokulvsvikram@gmail.com>
2026-06-28 20:29:34 +0530

    division withou / operator

---
+++ devision.py
@@ -0,0 +1,27 @@
+# division without using / operator
+
+def division(divident,divisor):
+    # checking zero division
+    if divisor == 0:
+        return "Division by zero not possible"
+    
+    quotient = 0
+    
+    # checking sign
+    
+    if divident<0 or divisor<0:
+        sign = -1
+    else:
+        sign = 1
+
+    # looping
+    while divident >= divisor:
+        divident -= divisor
+        quotient += 1
+
+    return sign*quotient, divident
+
+n1,n2 = division(43,9)
+
+n1 = print("Quotient :",n1)
+n2 = print("Remainder:",n2)
